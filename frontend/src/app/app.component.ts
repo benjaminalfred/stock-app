5efdc0d79c9a844cb282484b540fbae5341854b6
Peter Horton <phorton@softstackfactory.org>
2018-09-17 04:20:17 +0000

    added backend and ng-charts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { StocksService } from './stocks.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  //  title = 'alpha-vantage';
+  
+/*  is_valid;
+  phone_number;
+  carrier;
+  line_type; */
+
+  
+  constructor(private _look: StocksService) {}
+  
+    ngOnInit(){
+   
+ /*   this._look.getPhoneDetails()
+      .subscribe( (data:any) => {
+        this.is_valid = data.is_valid;
+        this.phone_number = data.phone_number;
+        this.carrier = data.carrier;
+        this.line_type = data.line_type
+      console.log(data)
+      console.log(this.is_valid)
+ */     })
+
+  } 
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { ClarityModule } from '@clr/angular';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { ClarityModule } from '@clr/angular';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { routes } from './app.router';
+import { MainComponent } from './main/main.component';
+import { ChartsModule } from 'ng2-charts';
+import { DayComponent } from './day/day.component';
+import { StocksService } from './stocks.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    DayComponent
   ],
   imports: [
     BrowserModule,
     ClarityModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    routes,
+    FormsModule,
+    ChartsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [StocksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
