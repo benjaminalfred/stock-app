99631176b7cf82302ab4b4c12c15f7aa1a0684ec
benjaminalfred <benjaminalfred@gmail.com>
2018-09-20 04:19:02 +0000

    added favorite collection

--- backend/common/models/app-user.js
+++ backend/common/models/app-user.js
@@ -1,5 +1,29 @@
 'use strict';
 
 module.exports = function(Appuser) {
+    
+    Appuser.afterRemote('login', (ctx, user, next) => {
+    if(user){
+      user.token = user.id;
+    }
+    next()
+  })
+  
+  Appuser.observe('after save', function(ctx, next) {
+        if (ctx.isNewInstance === true) {
+            var instance = ctx.instance;
+            
+            instance.createAccessToken(1209600000, function(err, response) {
+                if (err === null) {
+                    ctx.instance["userId"] = response.userId;
+                    ctx.instance["token"] = response.id;
+                    next();
+                }
+            });
+            
+        } else {
+            next();
+        }
+    });
 
 };
