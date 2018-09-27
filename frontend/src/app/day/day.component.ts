d6c89f296e392008524c1278beec59383fe0e66f
benjaminalfred <benjaminalfred@gmail.com>
2018-09-27 03:58:59 +0000

    able to update dates array

--- frontend/src/app/day/day.component.ts
+++ frontend/src/app/day/day.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from "@angular/core";
-import { StocksService } from "../stocks.service";
+import { Component, OnInit } from "@angular/core"
+import { StocksService } from "../stocks.service"
 
 @Component({
   selector: "app-day",
@@ -7,39 +7,40 @@ import { StocksService } from "../stocks.service";
   styleUrls: ["./day.component.css"]
 })
 export class DayComponent implements OnInit {
-  stockSymbol = "";
+  stockSymbol = ""
   constructor(private _stocks: StocksService) {}
 
-  stockPrice: any = [];
-  stockDate: any = [];
+  stockPrice: any = []
+  stockDate: any = []
 
   stockSearch() {
     this._stocks.getStock(this.stockSymbol).subscribe((data: any) => {
+      this.lineChartLabels.length = 0;
       for (let date in data["Monthly Adjusted Time Series"]) {
-        this.stockDate.push(date);
-        let wholePrice = data["Monthly Adjusted Time Series"][date]["4. close"];
-        let rndPrice = Math.round(wholePrice);
-        this.stockPrice.push(rndPrice);
+        this.lineChartLabels.push(date)
+        let wholePrice = data["Monthly Adjusted Time Series"][date]["4. close"]
+        let rndPrice = Math.round(wholePrice)
+        this.stockPrice.push(rndPrice)
       }
-      console.log(this.stockDate);
-      console.log(this.stockPrice);
+      console.log(this.stockDate)
+      console.log(this.stockPrice)
 
-      this.lineChartData = [{ data: this.stockPrice }];
-      //this.lineChartLabels = [{ data: this.stockDate }];
-    });
+      this.lineChartData = [{ data: this.stockPrice }]
+      this.lineChartLabels = this.stockDate;
+    })
   }
 
   ngOnInit() {
-    this.lineChartData = [{ data: [] }];
-    this.lineChartLabels = [{ data: [] }];
+    this.lineChartData = [{ data: [], label:[]}]
+    //this.lineChartLabels = []
   }
 
   // lineChart
-  public lineChartData: Array<any> = [];
-  public lineChartLabels: Array<any> = [];
+  public lineChartData: Array<any> = []
+  public lineChartLabels: Array<any> = []
   public lineChartOptions: any = {
     responsive: true
-  };
+  }
   /*public lineChartColors:Array<any> = [
     { // grey
       backgroundColor: 'rgba(148,159,177,0.2)',
@@ -65,30 +66,30 @@ export class DayComponent implements OnInit {
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     }
-  ];*/
-  public lineChartLegend: boolean = false;
-  public lineChartType: string = "line";
+  ]*/
+  public lineChartLegend: boolean = false
+  public lineChartType: string = "line"
 
   public randomize(): void {
-    let _lineChartData: Array<any> = new Array(this.lineChartData.length);
+    let _lineChartData: Array<any> = new Array(this.lineChartData.length)
     for (let i = 0; i < this.lineChartData.length; i++) {
       _lineChartData[i] = {
         data: new Array(this.lineChartData[i].data.length),
         label: this.lineChartData[i].label
-      };
+      }
       for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        _lineChartData[i].data[j] = Math.floor(Math.random() * 100 + 1);
+        _lineChartData[i].data[j] = Math.floor(Math.random() * 100 + 1)
       }
     }
-    this.lineChartData = _lineChartData;
+    this.lineChartData = _lineChartData
   }
 
   // events
   public chartClicked(e: any): void {
-    console.log(e);
+    console.log(e)
   }
 
   public chartHovered(e: any): void {
-    console.log(e);
+    console.log(e)
   }
 }
